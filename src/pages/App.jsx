ddadbb2b1d0c2f31ed01f5c392f54b0244f63458
lucarl07 <junitinho2007@gmail.com>
2024-10-15 19:17:28 -0300

    ✨ feat: Add & compose all components on the App page

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,7 +1,8 @@
-// Stylesheet:
-import './App.css';
+// Dependencies:
+import { useState } from 'react';
 
-// Assets:
+// Stylesheet & Assets:
+import './App.css';
 import IconArrow from "../assets/images/icon-arrow.svg"
 
 // Singular Components:
@@ -15,14 +16,60 @@ import Break from "../components/Break";
 import Output from "../components/Output";
 
 function App() {
+  const [day, setDay] = useState('')
+  const [month, setMonth] = useState('')
+  const [year, setYear] = useState('')
+
   return (
     <>
       <Container>
-        <h1>Olá mundo!</h1>
-        <Form></Form>
+        <Form>
+          <Input.Root>
+            <Input.Label 
+              type="day" 
+              hasError={false} />
+            <Input.Field
+              getter={day}
+              setter={setDay}
+              placeholder='dd'
+              hasError={false} />
+            <Input.Error message={""} />
+          </Input.Root>
+
+          <Input.Root>
+            <Input.Label 
+              type="month" 
+              hasError={false} />
+            <Input.Field
+              getter={month}
+              setter={setMonth}
+              placeholder='dd'
+              hasError={false} />
+            <Input.Error message={""} />
+          </Input.Root>
+
+          <Input.Root>
+            <Input.Label 
+              type="year" 
+              hasError={false} />
+            <Input.Field
+              getter={year}
+              setter={setYear}
+              placeholder='dd'
+              hasError={false} />
+            <Input.Error message={""} />
+          </Input.Root>
+        </Form>
+
         <Break.Root>
           <Break.Icon src={IconArrow} alt="Arrow" />
         </Break.Root>
+
+        <Results>
+          <Output value={0} period='day' />
+          <Output value={0} period='month' />
+          <Output value={0} period='year' />
+        </Results>
       </Container>
     </>
   )
