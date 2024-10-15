5bee910ccecfc85cb679d93ed3fedb3817669f37
lucarl07 <junitinho2007@gmail.com>
2024-10-15 19:13:55 -0300

    ♻ refactor: Transform Output into a singular component

--- src/components/Output/index.jsx
+++ src/components/Output/index.jsx
@@ -1,7 +1,22 @@
-import Root from "./OutputRoot"
-import Value from "./OutputValue"
-import Period from "./OutputPeriod"
+import PropTypes from "prop-types";
 
-const Output = { Root, Value, Period }
+const Output = ({value, period}) => {
+  return (
+    <h1 className="output">
+      <span className="number">
+        {value || "--"}
+      </span>
+      {" "}
+      <span className="period">
+        {Math.abs(value) != 1 ? period + 's' : period}
+      </span>
+    </h1>
+  );
+}
+
+Output.propTypes = {
+  value: PropTypes.number.isRequired,
+  period: PropTypes.string.isRequired
+}
 
 export default Output;
