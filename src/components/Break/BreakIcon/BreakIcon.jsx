8cc45b1c9722dfaba306c2da86995dc7f5927187
lucarl07 <junitinho2007@gmail.com>
2024-10-25 14:13:53 -0300

    💄 feat: Stylize Break component

--- src/components/Break/BreakIcon/BreakIcon.jsx
+++ src/components/Break/BreakIcon/BreakIcon.jsx
@@ -1,13 +1,12 @@
+// Stylesheet:
+import styles from './BreakIcon.module.css'
+
+// Dependencies:
 import PropTypes from "prop-types"
 
 const BreakIcon = ({src, alt}) => {
   return (
-    <div
-      className="icon"
-      style={{
-        width: "fit-content",
-        backgroundColor: "purple"
-      }}>
+    <div className={styles.icon}>
       <img src={src} alt={alt} />
     </div>
   );
